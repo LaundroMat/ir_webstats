899b92b02cc90bb05064017ce0c59e81ad775fd8
Mathieu <laundro@gmail.com>
2016-03-07 23:04:13 +0100

    Do not decode event_results from API
    Fixes https://github.com/jeysonmc/ir_webstats/issues/4
    
    Not tested on python 2.x

--- client.py
+++ client.py
@@ -459,8 +459,7 @@ class iRWebStats:
         """ Gets the event results (table of positions, times, etc.). The
             event is identified by a subsession id. """
 
-        r = self.__req(ct.URL_GET_EVENTRESULTS % (subsession, sessnum))\
-                .encode('utf8')
+        r = self.__req(ct.URL_GET_EVENTRESULTS % (subsession, sessnum))
         data = [x for x in csv.reader(StringIO(r), delimiter=',',
                                       quotechar='"')]
         header_ev, header_res = data[0], data[3]
